f6e01b69c17c1f0e7ee44ae1a3b837b09f37b85b
dev-nass <jonasvincemaca@gmail.com>
2025-03-02 01:12:49 +0800

    Navbar Done

--- resources/views/components/navbar.php
+++ resources/views/components/navbar.php
@@ -3,47 +3,18 @@
         <div class="container">
             <!-- Brand Name/logo -->
             <a id="brand" href="index.php">
-                <div class="mb-0"><img src="img/brand-logo.jpg" alt=""></div>
+                <h4 class="mb-0"><span class="logo-text-1">Norte</span> <span class="logo-text-2">Cafe</span></h4>
             </a>
 
             <!-- Menu -->
             <div id="navbar-collapse-toggle" class="collapse navbar-collapse justify-content-end">
-
-                <!-- Profile (Small screen) -->
-                <div class="ms-auto d-flex d-lg-none flex-column align-items-center justify-content-center mt-3">
-                    <div class="profile">
-                        <div class="dropdown-center">
-                            <button class="btn dropdown-toggle no-arrow p-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa-solid fa-user m-3"></i>
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a href="#" class="normal-font dropdown-item">Hello, Guest </a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li class="#"><a class="normal-font dropdown-item" href="php/previousOrderHistoryPage.php">Previous Order <i class="fa-solid fa-rotate-left"></i></a></li>
-                                <li class="#">
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li>
-                                    <form action="php/logout.php" method="POST">
-                                        <button type="submit" class="dropdown-item text-success">
-                                            Login<i class="fa-solid fa-right-from-bracket ms-1"></i>
-                                        </button>
-                                    </form>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
-                </div>
 
                 <ul class="navbar-nav mx-auto">
                     <li>
                         <a href="index.php" class="nav-link">Home</a>
                     </li>
                     <li>
-                        <a href="php/itemList.php" class="nav-link">Guitars</a>
+                        <a href="php/itemList.php" class="nav-link">Menu</a>
                     </li>
                     <li>
                         <a href="aboutUs.php" class="nav-link">About</a>
@@ -54,9 +25,18 @@
                 </ul>
 
                 <!-- <div id="div-search" class="ms-auto d-flex d-lg-none justify-content-center">
-            <input id="input-search" class="form-control w-auto me-2" type="search" placeholder="Search" aria-label="Search">
-            <button id="button-search" class="btn btn-outline-light" type="submit">Search</button>
-          </div> -->
+                    <input id="input-search" class="form-control w-auto me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button id="button-search" class="btn btn-outline-light" type="submit">Search</button>
+                    </div> -->
+                <!-- Profile (Small screen) -->
+                <div class="ms-auto d-flex d-lg-none flex-column align-items-center justify-content-center mt-3">
+                    <div class="profile">
+                        <a href="#" class="choco-btn">
+                            Sign Up
+                            <!-- <i class="fa-solid fa-user m-3"></i> -->
+                        </a>
+                    </div>
+                </div>
 
 
 
@@ -66,29 +46,10 @@
             <!-- Profile (Large scren) -->
             <div class="ms-auto d-none d-lg-flex align-items-center">
                 <div class="profile">
-                    <div class="dropdown-center">
-                        <button class="btn dropdown-toggle no-arrow p-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="fa-solid fa-user m-3"></i>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="normal-font dropdown-item" href="#">Hello, Guest </a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="normal-font dropdown-item" href="#">Previous Orders <i class="fa-solid fa-clock-rotate-left"></i></a></li>
-                            <li class="">
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li>
-                                <form action="php/logout.php" method="POST">
-                                    <button type="submit" class="dropdown-item text-success">
-                                        Login<i class="fa-solid fa-right-from-bracket ms-1"></i>
-                                    </button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-
+                    <a href="#" class="choco-btn">
+                        Sign Up
+                        <!-- <i class="fa-solid fa-user m-3"></i> -->
+                    </a>
                 </div>
             </div>
 
--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -1,6 +1,5 @@
 <?php require '../views/components/head.php' ?>
 <?php require '../views/components/navbar.php' ?>
 
-<h1 class="section-header">Hello From Index</h1>
 
 <?php require '../views/components/foot.php' ?>
