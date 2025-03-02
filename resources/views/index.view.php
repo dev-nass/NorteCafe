ff2c33ed6f9dc6f0f51313a51ce480ff46738966
dev-nass <jonasvincemaca@gmail.com>
2025-03-02 23:52:21 +0800

    Index Hero Done

--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -2,4 +2,59 @@
 <?php require '../views/components/navbar.php' ?>
 
 
+<!-- SECTION 1 (Hero Section) -->
+<section class="section-container">
+    <div class="container py-5">
+        <div class="row">
+
+            <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+                <img src="../../storage/user/img/hero-blob.png" class="hero-blob w-sm-50" />
+            </div>
+
+            <div class="col-12 col-md-6 py-4">
+                <div>
+                    <h3 class="hero-header">
+                        Discover the drinks and deserts that winds you to the right mood
+                    </h3>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur maxime quis culpa eveniet in dicta totam molestiae consequatur nostrum temporibus.</p>
+                </div>
+                <div class="mt-3 mb-4">
+                    <a href="#" class="choco-btn">
+                        Order Now
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                        <div class="d-flex">
+                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
+                            <div class="ms-2">Beat <br> the Heat</div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                        <div class="d-flex">
+                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
+                            <div class="ms-2">Beat <br> the Heat</div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                        <div class="d-flex">
+                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
+                            <div class="ms-2">Beat <br> the Heat</div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                        <div class="d-flex">
+                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
+                            <div class="ms-2">Beat <br> the Heat</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
 <?php require '../views/components/foot.php' ?>
