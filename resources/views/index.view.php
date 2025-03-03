0427b923598e3c7a3e981ee9ba49aee8c8724e0b
dev-nass <jonasvincemaca@gmail.com>
2025-03-03 22:31:05 +0800

    Draft Index Done

--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -26,30 +26,204 @@
                     </a>
                 </div>
                 <div class="row">
-                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                    <div class="col-6 col-lg-3 mt-2">
                         <div class="d-flex">
                             <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
                             <div class="ms-2">Beat <br> the Heat</div>
                         </div>
                     </div>
-                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                    <div class="col-6 col-lg-3 mt-2">
                         <div class="d-flex">
                             <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
                             <div class="ms-2">Beat <br> the Heat</div>
                         </div>
                     </div>
-                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                    <div class="col-6 col-lg-3 mt-2">
                         <div class="d-flex">
                             <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
                             <div class="ms-2">Beat <br> the Heat</div>
                         </div>
                     </div>
-                    <div class="col-sm-6 col-lg-3 mt-sm-2">
+                    <div class="col-6 col-lg-3 mt-2">
                         <div class="d-flex">
                             <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
                             <div class="ms-2">Beat <br> the Heat</div>
                         </div>
                     </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- SECTION 2 (Top Sales) -->
+<section class="section-container">
+    <div class="container">
+        <div>
+            <h1 class="section-header text-center">
+                Best Selling Products
+            </h1>
+        </div>
+        <div class="row">
+            <div class=" col-6 col-lg-3 mt-3">
+                <div class="card">
+                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Caramel Milktea</h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="mt-3">P80.00</p>
+                            <a href="#" class="btn dark-btn">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=" col-6 col-lg-3 mt-3">
+                <div class="card">
+                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Caramel Milktea</h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="mt-3">P80.00</p>
+                            <a href="#" class="btn dark-btn">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=" col-6 col-lg-3 mt-3">
+                <div class="card">
+                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Caramel Milktea</h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="mt-3">P80.00</p>
+                            <a href="#" class="btn dark-btn">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=" col-6 col-lg-3 mt-3">
+                <div class="card">
+                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Caramel Milktea</h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="mt-3">P80.00</p>
+                            <a href="#" class="btn dark-btn">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- SECTION 3 (Delivery Info) -->
+<section class="section-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h1 class="section-header delivery-header">Delivery Your Cravings at Your Doorstep</h1>
+                <p class="mt-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, facere in! Quidem nulla ratione incidunt tempore illo asperiores, voluptatem doloremque.</p>
+                <a href="#" class="choco-btn mt-4">Learn More</a>
+            </div>
+            <div class="col-12 col-md-6 mt-4 d-flex justify-content-center align-items-center">
+                <div>
+                    <img src="../../storage/user/img/delivery-logo.png" class="delivery-logo" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- SECTION 4 (We Value) -->
+<section class="section-container">
+    <div class="container">
+        <h1 class="section-header text-center">We Value</h1>
+        <div class="row border mt-4">
+            <div class="col-sm-6 col-lg-3">
+                <div class="d-flex justify-content-between align-items-center flex-row flex-lg-column text-sm-start text-lg-center">
+                    <div>
+                        <div class="cirlce-container me-2 me-sm-3 mb-lg-3">
+                            <i class="feature-icon fa fa-truck text-light"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="d-block mb-1 fw-bold">Fast & Free Shipping</p>
+                        <p>We deliver your guitar to your door with the utmost care and ease.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="d-flex justify-content-between align-items-center flex-row flex-lg-column text-sm-start text-lg-center">
+                    <div>
+                        <div class="cirlce-container me-2 me-sm-3 mb-lg-3">
+                            <i class="feature-icon fa fa-lock text-light"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="d-block mb-1 fw-bold">Secure Payment</p>
+                        <p>Shop confidently with secure transactions protecting your privacy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="d-flex justify-content-between align-items-center flex-row flex-lg-column text-sm-start text-lg-center">
+                    <div>
+                        <div class="cirlce-container me-2 me-sm-3 mb-lg-3">
+                            <i class="feature-icon fa fa-heart text-light"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="d-block mb-1 fw-bold">Customer Satisfaction</p>
+                        <p>Your happiness is our priority, ensuring quality service always.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="d-flex justify-content-between align-items-center flex-row flex-lg-column text-sm-start text-lg-center">
+                    <div>
+                        <div class="cirlce-container me-2 me-sm-3 mb-lg-3">
+                            <i class="feature-icon fa fa-comments text-light"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="d-block mb-1 fw-bold">Customer Support</p>
+                        <p> We're here to assist you anytime, ensuring your needs are met.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- SECTION 5 (Our Story) -->
+<section class="section-container">
+    <div class="container py-5">
+        <div class="row">
+
+            <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+                <img src="../../storage/user/img/our-story.jpg" class="our-story-img w-sm-50" />
+            </div>
+
+            <div class="col-12 col-md-6 py-4">
+                <div>
+                    <h1 class="section-header">
+                        Our Story
+                    </h1>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptas provident in, ratione est, facere suscipit sit nisi veniam nemo, enim voluptatum vero ab dolore quia accusamus unde voluptate quasi.</p>
+                </div>
+                <div class="mt-3 mb-4">
+                    <a href="#" class="choco-btn">
+                        Learn More
+                    </a>
                 </div>
             </div>
 
