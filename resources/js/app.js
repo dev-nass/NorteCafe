4df2cb2044ffa0e1145dbd63b4a30a74570e1a82
dev-nass <jonasvincemaca@gmail.com>
2025-03-04 22:23:52 +0800

    Index Done

---
+++ resources/js/app.js
@@ -0,0 +1,2 @@
+
+import ('scrollReveal.min.js');
--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -3,51 +3,52 @@
 
 
 <!-- SECTION 1 (Hero Section) -->
-<section class="section-container">
+<section class="section-container white-bg">
     <div class="container py-5">
         <div class="row">
 
             <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
-                <img src="../../storage/user/img/hero-blob.png" class="hero-blob w-sm-50" />
+                <img src="../../storage/user/img/hero-icon-white.png" class="hero-grid" />
             </div>
 
             <div class="col-12 col-md-6 py-4">
                 <div>
                     <h3 class="hero-header">
-                        Discover the drinks and deserts that winds you to the right mood
+                        Discover the drinks and desserts that set you in the right mood.
                     </h3>
                 </div>
-                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur maxime quis culpa eveniet in dicta totam molestiae consequatur nostrum temporibus.</p>
+                <div class="mt-2 mb-4">
+                    <p>Explore a variety of rich, handcrafted coffees, delightful pastries, and savory treats. From classic brews to sweet indulgences, there’s something for every craving.</p>
                 </div>
                 <div class="mt-3 mb-4">
                     <a href="#" class="choco-btn">
                         Order Now
                     </a>
+                    <a href="#" class="btn text-primary ms-2">Contact Us</a>
                 </div>
                 <div class="row">
-                    <div class="col-6 col-lg-3 mt-2">
+                    <div class="col-6 mt-2">
                         <div class="d-flex">
-                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
-                            <div class="ms-2">Beat <br> the Heat</div>
+                            <div class="square"><i class="fa-solid fa-door-open h4 square-icon"></i></div>
+                            <div class="ms-2">Open Everday <br> (Except Holidays)</div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 mt-2">
+                    <div class="col-6 mt-2">
                         <div class="d-flex">
-                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
-                            <div class="ms-2">Beat <br> the Heat</div>
+                            <div class="square"><i class="fa-solid fa-clock h4 square-icon"></i></div>
+                            <div class="ms-2">Open From <br> 9am-10pm</div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 mt-2">
+                    <div class="col-6 mt-2">
                         <div class="d-flex">
-                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
-                            <div class="ms-2">Beat <br> the Heat</div>
+                            <div class="square"><i class="fa-solid fa-utensils h4 square-icon"></i></div>
+                            <div class="ms-2">For <br> Dine In</div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 mt-2">
+                    <div class="col-6 mt-2">
                         <div class="d-flex">
-                            <div class="square"><i class="fa-solid fa-sun h4 square-icon"></i></div>
-                            <div class="ms-2">Beat <br> the Heat</div>
+                            <div class="square"><i class="fa-solid fa-truck h4 square-icon"></i></div>
+                            <div class="ms-2">Online <br> Delivery</div>
                         </div>
                     </div>
                 </div>
@@ -57,81 +58,95 @@
     </div>
 </section>
 
-<!-- SECTION 2 (Top Sales) -->
+<!-- SECTION 2 (Explore Our Menu) -->
 <section class="section-container">
     <div class="container">
         <div>
-            <h1 class="section-header text-center">
-                Best Selling Products
-            </h1>
+            <h1 class="section-header text-center">Explore Our Menu</h1>
         </div>
+
         <div class="row">
-            <div class=" col-6 col-lg-3 mt-3">
-                <div class="card">
-                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Caramel Milktea</h5>
-                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <p class="mt-3">P80.00</p>
-                            <a href="#" class="btn dark-btn">Buy Now</a>
+            <div class="col-12 col-lg-6 d-flex justify-content-center">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-4">
+                            <img src="../../storage/user/img/croffle.jpg" class="img-fluid rounded-start h-100" alt="...">
+                        </div>
+                        <div class="col-8 choco-bg">
+                            <div class="card-body">
+                                <h5 class="card-title">Croffle</h5>
+                                <p class="card-text mt-2 mb-4">A crispy, flaky croissant pressed like a waffle, sweet or savory.</p>
+                                <a href="#" class="white-choco-btn">View Product</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class=" col-6 col-lg-3 mt-3">
-                <div class="card">
-                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Caramel Milktea</h5>
-                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <p class="mt-3">P80.00</p>
-                            <a href="#" class="btn dark-btn">Buy Now</a>
+            <div class="col-12 col-lg-6 d-flex justify-content-center">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-8 choco-bg">
+                            <div class="card-body">
+                                <h5 class="card-title">Sandwhich & Nachos</h5>
+                                <p class="card-text mt-2 mb-4">Crunchy nachos and hearty sandwiches, packed with flavors.</p>
+                                <a href="#" class="white-choco-btn">View Product</a>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class=" col-6 col-lg-3 mt-3">
-                <div class="card">
-                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Caramel Milktea</h5>
-                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <p class="mt-3">P80.00</p>
-                            <a href="#" class="btn dark-btn">Buy Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class=" col-6 col-lg-3 mt-3">
-                <div class="card">
-                    <img src="../../storage/user/img/milktea-md-lg.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Caramel Milktea</h5>
-                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, doloribus.</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <p class="mt-3">P80.00</p>
-                            <a href="#" class="btn dark-btn">Buy Now</a>
+                        <div class="col-4">
+                            <img src="../../storage/user/img/sandwhich-nachos.jpg" class="img-fluid rounded-start h-100" alt="...">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+
+        <div class="row">
+            <div class="col-12 col-lg-6 d-flex justify-content-center">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-4">
+                            <img src="../../storage/user/img/coffee.jpg" class="img-fluid rounded-start h-100" alt="...">
+                        </div>
+                        <div class="col-8 dark-bg">
+                            <div class="card-body">
+                                <h5 class="card-title">Coffee</h5>
+                                <p class="card-text mt-2 mb-4">Rich, aromatic coffee brewed to perfection for a delightful experience for everybody.</p>
+                                <a href="#" class="white-choco-btn">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 d-flex justify-content-center">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-8 dark-bg">
+                            <div class="card-body">
+                                <h5 class="card-title">Milktea</h5>
+                                <p class="card-text mt-2 mb-4">Creamy, flavorful, savoring milk tea with chewy boba for a refreshing treat.</p>
+                                <a href="#" class="white-choco-btn">View Product</a>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <img src="../../storage/user/img/milktea.jpg" class="img-fluid rounded-start h-100" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 </section>
 
 <!-- SECTION 3 (Delivery Info) -->
-<section class="section-container">
+<section class="section-container white-bg py-5">
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-6">
-                <h1 class="section-header delivery-header">Delivery Your Cravings at Your Doorstep</h1>
-                <p class="mt-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, facere in! Quidem nulla ratione incidunt tempore illo asperiores, voluptatem doloremque.</p>
+                <h1 class="section-header delivery-header">Delivery Your Cravings at ⤦ Your Doorstep</h1>
+                <p class="mt-3">Satisfy your cravings with fast, fresh, and hassle-free online delivery. Enjoy delicious meals, snacks, and drinks brought straight to your doorstep with just a few clicks!</p>
                 <a href="#" class="choco-btn mt-4">Learn More</a>
             </div>
-            <div class="col-12 col-md-6 mt-4 d-flex justify-content-center align-items-center">
+            <div class="col-12 col-md-6 mt-sm-5 mt-md-4 d-flex justify-content-center align-items-center">
                 <div>
                     <img src="../../storage/user/img/delivery-logo.png" class="delivery-logo" />
                 </div>
@@ -203,7 +218,7 @@
 
 
 <!-- SECTION 5 (Our Story) -->
-<section class="section-container">
+<section class="section-container white-bg">
     <div class="container py-5">
         <div class="row">
 
@@ -214,11 +229,11 @@
             <div class="col-12 col-md-6 py-4">
                 <div>
                     <h1 class="section-header">
-                        Our Story
+                        Story behind Norte Cafe by Cafe Buny
                     </h1>
                 </div>
                 <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptas provident in, ratione est, facere suscipit sit nisi veniam nemo, enim voluptatum vero ab dolore quia accusamus unde voluptate quasi.</p>
+                    <p>Founded by two kindred hearts, our coffee shop is a tale of love, warmth, and shared dreams—brewing passion into every cup for you to enjoy. What started as a simple dream became a cozy haven where every sip tells a story, inviting you to experience comfort, connection, and heartfelt flavors.</p>
                 </div>
                 <div class="mt-3 mb-4">
                     <a href="#" class="choco-btn">
