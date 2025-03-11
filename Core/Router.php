f5269b6042781e525999f93cdae4bfd48fd64270
dev-nass <jonasvincemaca@gmail.com>
2025-03-11 20:49:04 +0800

    Initial Backend Done

---
+++ Core/Router.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Core;
+
+use App\Http\Controllers;
+
+class Router {
+
+    public $routes = [];
+
+    public function add($method, $uri, $controllerClass, $controllerMethod) {
+        $this->routes = [
+            'method' => $method,
+            'uri' => "/PHP 2025/Norte Caffee/public/index.php/{$uri}",
+            'controller_class' => $controllerClass,
+            'controller_method' => $controllerMethod,
+        ];
+    }
+
+    public function get($uri, $controllerClass, $controllerMethod) {
+        return $this->add('GET', $uri, $controllerClass, $controllerMethod);
+    }
+
+    public function post($uri, $controllerClass, $controllerMethod) {
+        return $this->add('POST', $uri, $controllerClass, $controllerMethod);
+    }
+
+    public function delete($uri, $controllerClass, $controllerMethod) {
+        return $this->add('DELETE', $uri, $controllerClass, $controllerMethod);
+    }
+
+    /**
+     * PATCH - update part of a resource (if available and appropriate)
+    */
+    public function patch($uri, $controllerClass, $controllerMethod) {
+        return $this->add('PATCH', $uri, $controllerClass, $controllerMethod);
+    }
+
+    /**
+     * PUT - update a resource (by replacing it with a new version)
+    */
+    public function put($uri, $controllerClass, $controllerMethod) {
+        return $this->add('PUT', $uri, $controllerClass, $controllerMethod);
+    }
+
+    /**
+     * Iterates to every element of the 'public $routes = []' and create an instance of that
+     * controller class.
+    */
+    public function routes($uri, $method) {
+        foreach($this->routes as $route) {
+            if($route['uri'] === $uri && $route['controller_method'] === $method) {
+
+                $controllerClass = 'app\Http\Controllers\\' . $route['controller_class'];
+                $controllerInstance = new $controllerClass();
+                $controllerMethods = get_class_methods($controllerInstance);
+
+                foreach($controllerMethods as $controller_method) {
+                    if($route['controller_method'] === $controller_method) {
+                        call_user_func([$controllerInstance, $controller_method]);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ resources/views/components/admin_foot.php
@@ -0,0 +1,17 @@
+<!--   Assests/Core JS Files   -->
+<script src="../../assets/js/core/popper.min.js"></script>
+<script src="../../assets/js/core/bootstrap.min.js"></script>
+<script src="../../assets/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="../../assets/js/plugins/smooth-scrollbar.min.js"></script>
+<script src="../../assets/js/plugins/chartjs.min.js"></script>
+
+<!-- My Personal JS -->
+<script src="../../js/app.js" type="module"></script>
+
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+<script src="../assets/js/material-dashboard.min.js?v=3.2.0"></script>
+</body>
+
+</html>
